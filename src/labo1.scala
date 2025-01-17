937816c4934bfea5fa689b7e2bda6f7b2965fa90
scuhadar <scuhadar22@gmail.com>
2025-01-17 14:22:59 +0100

    updated

---
+++ src/labo1.scala
@@ -0,0 +1,18 @@
+object labo1 extends App {
+  //hello world
+  val word:String="Hello World"
+  println(word)
+
+  // Declares two values
+  val toto : Int = 3
+  val titi : Int = 12
+
+  //Computes the sum of two values
+  val theSum = toto + titi
+
+  //Display the result
+  print("The sum is equal to : ")
+  print(theSum)
+
+}
+
